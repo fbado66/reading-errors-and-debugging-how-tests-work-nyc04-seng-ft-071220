33fec4d31c6fa4b7cc6fa24c2638c0a10a9babea
Franklin Bado <fbado66@gmail.com>
2020-07-14 21:26:50 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = 1
-second_number = 2
+second_number != 0 
 sum = first_number + second_number
 difference = - second_number + first_number
 product = first_number * second_number
